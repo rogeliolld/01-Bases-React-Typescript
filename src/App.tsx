bbadd5b9a94b4962bf548f48a09674318c4b4953
Rogelio Llerena <rogelio.llerena@ceiba.com.co>
2022-01-25 17:10:31 -0500

    Agregando 'useLayoutEffect' y gsp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Counter } from "./bases/Counter";
 import { CounterBy } from "./bases/CounterBy";
+import { CounterEffect } from "./bases/CounterEffect";
+import { CounterHook } from "./bases/CounterHook";
 function App() {
   return (
     <>
       <h1>React</h1>
       <Counter initialValue={15} />
       <CounterBy />
+      <CounterEffect />
+      <CounterHook />
       <hr />
     </>
   );
---
+++ src/bases/CounterHook.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useCounter } from "../hooks/useCounter";
+
+export const CounterHook = () => {
+  const { counter, elementToAnimate, handleClick } = useCounter({
+    maxCount: 15,
+  });
+
+  return (
+    <>
+      <h1>CounterHook:</h1>
+      <h2 ref={elementToAnimate}>{counter}</h2>
+
+      <button onClick={handleClick}>+1</button>
+    </>
+  );
+};
